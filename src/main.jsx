f59ba7e34bb3ee8d1ddb2eda8af518e770959286
WEBHM2-MAIN1-MAIN 2 <taharami14@gmail.com>
2025-01-25 14:26:19 +0200

    first commit

--- src/main.jsx
+++ src/main.jsx
@@ -5,6 +5,8 @@ import "./index.css";
 import Home from "../src/components/home.jsx";
 import Login from "../src/components/login.jsx";
 import Mainpage from "../src/components/mainpage.jsx";
+import Notes from "../src/components/notes.jsx";
+import Addnote from "../src/components/addnote.jsx";
 
 const router = createBrowserRouter([
   {
@@ -22,6 +24,14 @@ const router = createBrowserRouter([
   {
     path: "/mainpage",
     element: <Mainpage />,
+  },
+  {
+    path: "/notes",
+    element: <Notes />,
+  },
+  {
+    path: "/addnote",
+    element: <Addnote />,
   },
 ]);
 
