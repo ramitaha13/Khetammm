b34b0853a59a2f052214d65e574dbe3625a15057
WEBHM2-MAIN1-MAIN 2 <taharami14@gmail.com>
2025-01-25 14:40:39 +0200

    first commit

--- src/main.jsx
+++ src/main.jsx
@@ -7,6 +7,11 @@ import Login from "../src/components/login.jsx";
 import Mainpage from "../src/components/mainpage.jsx";
 import Notes from "../src/components/notes.jsx";
 import Addnote from "../src/components/addnote.jsx";
+import Importantdates from "../src/components/importantdates.jsx";
+import Adddate from "../src/components/adddate.jsx";
+import Nowdate from "../src/components/nowdate.jsx";
+import Numbers from "../src/components/numbers.jsx";
+import Addnumbers from "../src/components/addnumbers.jsx";
 
 const router = createBrowserRouter([
   {
@@ -32,6 +37,26 @@ const router = createBrowserRouter([
   {
     path: "/addnote",
     element: <Addnote />,
+  },
+  {
+    path: "/importantdates",
+    element: <Importantdates />,
+  },
+  {
+    path: "/adddate",
+    element: <Adddate />,
+  },
+  {
+    path: "/nowdate",
+    element: <Nowdate />,
+  },
+  {
+    path: "/numbers",
+    element: <Numbers />,
+  },
+  {
+    path: "/addnumbers",
+    element: <Addnumbers />,
   },
 ]);
 
